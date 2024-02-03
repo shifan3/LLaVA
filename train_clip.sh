ebe96c9029d9a97f6113698f02e02ce937df1060
shifan3 <shifan3rd@gmail.com>
2024-02-03 13:23:18 +0800

    asda

--- train_clip.sh
+++ train_clip.sh
@@ -1,4 +1,4 @@
-export CLIP_MODEL=openai/clip-vit-large-patch14 #openai/clip-vit-large-patch14-336
+export CLIP_MODEL=openai/clip-vit-large-patch14-336
 export CLIP_MODEL_NAME=$(echo $CLIP_MODEL | cut -f2 -d/)
 python3.10 scripts/clip/train_clip_prepare.py $CLIP_MODEL
 OUTPUT_DIR=./checkpoints/$CLIP_MODEL_NAME-knowledge
